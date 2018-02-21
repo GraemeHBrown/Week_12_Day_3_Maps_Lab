368609d61ca3df55415a8d24ec6867b7bd753cc6
Robert Henderson <robert@nterpryz.com>
2018-02-21 15:08:10 +0000

    name, population & capital now dispaying correct info from the country object

--- public/index.js
+++ public/index.js
@@ -1,36 +1,50 @@
+
 const app = function(){
   const url    = "https://restcountries.eu/rest/v2/all"
   const select = document.getElementById('country-select');
-
-  const getCountriesSelectClicked = function(){
-    makeRequest(url,requestComplete)
-   }
-
-  select.addEventListener('click', getCountriesSelectClicked);
+  makeRequest(url,requestComplete)
 }
 
 const makeRequest = function (url, callback) {
- const request = new XMLHttpRequest();
+ const request    = new XMLHttpRequest();
  request.open("GET", url);
  request.addEventListener("load", callback);
  request.send(); //sending request to the server
+
 }
 
 const requestComplete = function () {
  if(this.status !== 200) return;  //404 cant find it, 500 server error, 200 everything ok and carries on. this.status same as saying request.status
  const jsonString = this.responseText;
- const countries = JSON.parse(jsonString);
+ const countries  = JSON.parse(jsonString);
  populateList(countries);
 }
 
 const populateList = function(countries){
-  const select = document.getElementById("country-select")
+  const select     = document.getElementById("country-select")
 
-  countries.forEach(function(country){
-    const option = document.createElement("option");
+  countries.forEach(function(country, index){
+    const option     = document.createElement("option");
     option.innerText = country.name;
+    option.value     = index
     select.appendChild(option);
   })
+
+  const getCountrySelectClicked = function(){
+    const selectedCountry = countries[this.value]
+
+    const countryName = document.getElementById('country-name')
+    countryName.innerText = selectedCountry.name
+
+    const countryPopulation = document.getElementById('country-population')
+    countryPopulation.innerText = selectedCountry.population
+
+    const countryCapital = document.getElementById('country-capital')
+    countryCapital.innerText = selectedCountry.capital
+
+
+  }
+  select.addEventListener('change', getCountrySelectClicked);
 }
 
 
